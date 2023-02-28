cec9d065a64f397991f75cbc8d6bab041582e2e4
hemanthsai k <khemanthsai01@gmail.com>
2023-02-28 15:06:31 +0530

    modified src and installed node pakages
    added components folder to src directary and added bootstrap and react-bootstrap pakages

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar className="navbar bg-gray">
+                <div className="container-fluid  p-2">
+                    <a className="navbar-brand mb-0 h1" href="/">Navbar</a>
+                </div>
+                <div>
+                    <Link to="/login" className="navbar-brand">
+                        Login
+                    </Link>
+                    <Link to="/signup" className="navbar-brand ">
+                        SignUp
+                    </Link>
+                </div>
+            </Navbar>
+
+
+        </div>
+    )
+}
+
+export default Home
