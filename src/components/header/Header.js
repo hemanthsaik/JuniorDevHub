b85a15123063d0caa0d520d18bb8a6dc247ffb4b
Axel <martin.axel@sfr.fr>
2023-03-16 09:34:41 +0100

    Fix React Routing on App.js and index.js, create header component and add it to all pages

---
+++ src/components/header/Header.js
@@ -0,0 +1,23 @@
+import { Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import React from 'react'
+
+
+export default function Header() {
+  return (
+      <Navbar className="navbar bg-light ">
+        <div className="container m-auto  p-2">
+          <Link className="navbar-brand mb-0 h1" href="/">JuniorDevHub</Link>
+        </div>
+        <div>
+          <Link to="/login" className="navbar-brand px-2">
+            Login
+          </Link>
+          <Link to="/signup" className="navbar-brand px-2 ">
+            SignUp
+          </Link>
+        </div>
+      </Navbar>
+  )
+}
--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { toast, ToastContainer } from 'react-toastify';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 
